bb0831b011f334c50f73cbd37053bb4497392216
paulina01fdz <fpaulina545@gmail.com>
2025-06-01 09:16:11 -0500

    Estructuracion de carpetas

---
+++ index.js
@@ -0,0 +1,13 @@
+const exp = require('express');
+const app = exp();
+require('dotenv').config();
+
+const logger = require('morgan');
+app.use(logger('dev'));
+
+app.use(exp.urlencoded({extended: false}));
+app.use(exp.json());
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor en línea');
+});
